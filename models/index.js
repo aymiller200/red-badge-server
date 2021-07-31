21e3f2235e0508f39e3be2707ead3da6e3215fa8
aymiller200 <aymiller200@gmail.com>
2021-07-31 01:52:26 -0400

    still trying to get tables to work

--- models/index.js
+++ models/index.js
@@ -1,7 +1,14 @@
 const Sequelize = require('sequelize')
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, { 
-    dialect: 'postgres'
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true, 
+            rejectUnauthorized: false
+        }
+    }
 })
 
 const db = {}
