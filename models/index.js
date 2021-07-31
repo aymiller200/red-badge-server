721222bc7b83599824acbd1d8cab4053e80a5d4b
aymiller200 <aymiller200@gmail.com>
2021-07-31 02:23:08 -0400

    still. not. working.

--- models/index.js
+++ models/index.js
@@ -2,17 +2,9 @@ const Sequelize = require('sequelize')
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, { 
     dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true, 
-            rejectUnauthorized: false
-        }
-    }
 })
 
 const db = {}
-
 db.Sequelize = Sequelize; 
 db.sequelize = sequelize; 
 
